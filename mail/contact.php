a25224b960e182b711f0059cde7f05e29e08e6d2
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-28 20:11:11 -0300

    ok

---
+++ mail/contact.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+  echo(var_dump($_POST));
+
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "info@example.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
